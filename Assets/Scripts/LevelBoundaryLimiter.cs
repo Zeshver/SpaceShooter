402790f52b4761718533496a6ac2977587f9eb89
Zeshver <saretert@gmail.com>
2022-11-26 00:50:35 +0300

    Added level boundary

---
+++ Assets/Scripts/LevelBoundaryLimiter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class LevelBoundaryLimiter : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (LevelBoundary.Instance == null) return;
+
+            var levelBoundary = LevelBoundary.Instance;
+            var radius = levelBoundary.Radius;
+
+            if (transform.position.magnitude > radius)
+            {
+                if (levelBoundary.LimitMode == LevelBoundary.Mode.Limit)
+                {
+                    transform.position = transform.position.normalized * radius;
+                }
+
+                if (levelBoundary.LimitMode == LevelBoundary.Mode.Teleport)
+                {
+                    transform.position = -transform.position.normalized * radius;
+                }
+            }
+        }
+    }
+}
