5a04e35fe3f77a26ec2297963e6008671ec47c45
Zeshver <saretert@gmail.com>
2022-11-27 13:40:03 +0300

    Added black hole
    Added script: Rotator, Teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class Teleporter : MonoBehaviour
+    {
+        [SerializeField] private Teleporter target;
+
+        [SerializeField] private bool IsReceive;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (IsReceive == true)
+            {
+                return;
+            }            
+
+            if (collision != null)
+            {
+                target.IsReceive = true;
+
+                collision.transform.position = target.transform.position;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision != null)
+            {
+                IsReceive = false;
+            }
+        }
+    }
+}
