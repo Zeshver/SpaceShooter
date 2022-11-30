be6752ce180e007ee1f8bab4d2859a866d25424a
Zeshver <saretert@gmail.com>
2022-11-30 14:27:04 +0300

    Added projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+namespace SpaceShooter
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Projectile : Entity
     {
-        
-    }
+        [SerializeField] private float m_Velocity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private float m_Lifetime;
+
+        [SerializeField] private int m_Damage;
+
+        [SerializeField] private ImpactEffect m_ImpactEffect;
+
+        private Destructible m_ParentDest;
+
+        private float m_Timer;
+
+        private void Update()
+        {
+            float stepLenght = Time.deltaTime * m_Velocity;
+            Vector2 step = transform.up * stepLenght;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, stepLenght);
+
+            if (hit)
+            {
+                Destructible dest = hit.collider.transform.root.GetComponent<Destructible>();
+
+                if (dest != null && dest != m_ParentDest)
+                {
+                    dest.ApplyDamage(m_Damage);
+                }
+
+                OnProjectileLifeEnd(hit.collider, hit.point);
+            }
+
+            m_Timer += Time.deltaTime;
+
+            if (m_Timer > m_Lifetime)
+            {
+                Destroy(gameObject);
+            }
+
+            transform.position += new Vector3(step.x, step.y, 0);
+        }
+
+        private void OnProjectileLifeEnd(Collider2D col, Vector2 pos)
+        {
+            Destroy(gameObject);
+        }
+
+        public void SetParentShooter(Destructible parent)
+        {
+            m_ParentDest = parent;
+        }
     }
 }
