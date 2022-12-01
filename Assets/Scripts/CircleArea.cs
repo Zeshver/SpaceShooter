a7aa02c5ac5736b8f9db55c89bfc4c3a9120515b
Zeshver <saretert@gmail.com>
2022-12-02 01:29:26 +0300

    Added script CircleArea

---
+++ Assets/Scripts/CircleArea.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace SpaceShooter
+{
+    public class CircleArea : MonoBehaviour
+    {
+        [SerializeField] private float m_Radius;
+        public float Radius => m_Radius;
+
+        public Vector2 GetRandomInsideZone()
+        {
+            return transform.position + Random.insideUnitSphere * m_Radius;
+        }
+#if UNITY_EDITOR
+        private static Color GizmoColor = new Color(0, 1, 0, 0.3f);
+
+        private void OnDrawGizmosSelected()
+        {
+            Handles.color = GizmoColor;
+            Handles.DrawSolidDisc(transform.position, transform.forward, m_Radius);
+        }
+#endif
+    }
+}
